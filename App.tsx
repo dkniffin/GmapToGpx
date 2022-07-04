bab34f6b7854c86a527a4f9108aed54a86508575
Derek Kniffin <derek.kniffin@gmail.com>
2022-07-04 11:41:12 -0400

    Set up a UI with input and button

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: "#fff"
+  },
+  input: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    borderColor: "#fff",
+    color: "#fff",
+  }
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Please enter a Google Maps URL</Text>
+      <TextInput
+        style={styles.input}
+        // onChangeText={onChangeText}
+        // value={text}
+      />
+      <Button
+        // onPress={onPressLearnMore}
+        title="Generate GPX"
+        color="#32a852"
+      />
+    </View>
+  );
+}
